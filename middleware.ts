ce149f8794d0ce7f0b3508cb0f0de5284c1d5071
lautapercuspain <lautapercuspain@gmail.com>
2023-05-15 13:37:23 -0700

    midd,eware

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function middleware(request: NextRequest) {
+  let ip = request.ip ?? request.headers.get("x-real-ip")
+  const forwardedFor = request.headers.get("x-forwarded-for")
+  if (!ip && forwardedFor) {
+    ip = forwardedFor.split(",").at(0) ?? "Unknown"
+  }
+  console.log("ip::", ip)
+  console.log("request", request)
+
+  const response = NextResponse.next()
+
+  if (ip) {
+    response.cookies.set("user-ip", ip, {
+      httpOnly: false,
+    })
+  }
+  return response
+}
